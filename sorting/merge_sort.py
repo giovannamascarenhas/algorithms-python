6fbd09446699d5eb9de810782098acb923662cba
giovannamascarenhas <giovanna.mascarenhas.1@gmail.com>
2020-11-19 15:33:02 +0000

    New script for merge sort algorithm

---
+++ sorting/merge_sort.py
@@ -0,0 +1,34 @@
+def merge(left, right):
+    if len(left) == 0:
+        return right
+    if len(right) == 0:
+        return left
+    index_left = index_right = 0
+    result = []
+    while len(result) < len(left) + len(right):
+        if left[index_left] <= right[index_right]:
+            result.append(left[index_left])
+            index_left += 1
+        else:
+            result.append(right[index_right])
+            index_right += 1
+        if index_right == len(right):
+            result += left[index_left:]
+            break
+        if index_left == len(left):
+            result += right[index_right:]
+            break
+    return result
+
+
+def merge_sort(array):
+    if len(array) < 2:
+        return array
+    midpoint = len(array) // 2
+    return merge(
+        left=merge_sort(array[:midpoint]),
+        right=merge_sort(array[midpoint:])
+    )
+
+
+array = [8,2,6,4,5]
